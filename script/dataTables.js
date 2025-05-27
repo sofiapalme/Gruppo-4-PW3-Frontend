af8a4eab7633094906891a9cbe268ee060b25713
fabioporzio <fabio.porzio00@gmail.com>
2025-05-27 15:34:59 +0200

    Added more fetch functions

--- script/dataTables.js
+++ script/dataTables.js
@@ -1,4 +1,138 @@
+function getIdoDate(inputDate) {
+    const formattedDate = inputDate.toISOString().split('T')[0];
+    return formattedDate;
+}
+
+function convertToItalianDate(isoDate) {
+  const date = new Date(isoDate);
+  const day = String(date.getDate()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const year = date.getFullYear();
+  return `${day}/${month}/${year}`;
+}
+
 let contactListTableInstance = null;
+
+const visiteFutureNavList = document.getElementById("visualizza-elenco-visite-future");
+visiteFutureNavList.addEventListener("click", createVisiteFutureDataTable);
+
+async function createVisiteFutureDataTable() {
+    const url = "http://localhost:8080/visit";
+    const accessToken = localStorage.getItem("accessToken");
+
+    try {
+        const response = await fetch(url, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer " + accessToken
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error(`Errore nel recupero dei dati: ${response.status}`);
+        }
+
+        const data = await response.json();
+
+        if (contactListTableInstance) {
+            contactListTableInstance.destroy();
+            contactListTableInstance = null;
+        }
+
+        const futureVisits = new Array;
+        const today = getIdoDate(new Date);
+        
+        data.forEach(visit => {
+            if (visit.dataInizio > today) {
+                visit.dataInizio = convertToItalianDate(visit.dataInizio);
+                visit.dataFine = convertToItalianDate(visit.dataFine);
+                futureVisits.push(visit)
+            }
+        });
+        console.log(futureVisits)
+
+        contactListTableInstance = new DataTable("#tabella-visite-future", {
+            data: futureVisits,
+            destroy: true,
+            columns: [
+                { data: "dataInizio" },
+                { data: "dataFine" },
+                { data: "oraInizio" },
+                { data: "oraFine" },
+                { data: "motivo" },
+                { data: "personaVisitatore.nome" },
+                { data: "responsabile.nome" },
+                { data: "flagDPI" },
+                { data: "materialeInformatico.id" },
+                { data: "flagAccessoConAutomezzo" }
+            ]
+        });
+    } catch (error) {
+        console.error("Errore nella creazione della tabella:", error.message);
+    }
+}
+
+const visiteOrdierneNavList = document.getElementById("visualizza-elenco-visite-odierne");
+visiteOrdierneNavList.addEventListener("click", createVisiteOdierneDataTable);
+
+async function createVisiteOdierneDataTable() {
+    const url = "http://localhost:8080/visit";
+    const accessToken = localStorage.getItem("accessToken");
+
+    try {
+        const response = await fetch(url, {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer " + accessToken
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error(`Errore nel recupero dei dati: ${response.status}`);
+        }
+
+        const data = await response.json();
+
+        if (contactListTableInstance) {
+            contactListTableInstance.destroy();
+            contactListTableInstance = null;
+        }
+
+        const todayVisits = new Array;
+        const today = getIdoDate(new Date);
+        
+        data.forEach(visit => {
+            if (visit.dataInizio === today) {
+                visit.dataInizio = convertToItalianDate(visit.dataInizio);
+                visit.dataFine = convertToItalianDate(visit.dataFine);
+                todayVisits.push(visit)
+            }
+        });
+        console.log(todayVisits)
+
+        contactListTableInstance = new DataTable("#tabella-visite-odierne", {
+            data: todayVisits,
+            destroy: true,
+            columns: [
+                { data: "dataInizio" },
+                { data: "dataFine" },
+                { data: "oraInizio" },
+                { data: "oraFine" },
+                { data: "motivo" },
+                { data: "personaVisitatore.nome" },
+                { data: "responsabile.nome" },
+                { data: "flagDPI" },
+                { data: "materialeInformatico.id" },
+                { data: "flagAccessoConAutomezzo" }
+            ]
+        });
+
+    } catch (error) {
+        console.error("Errore nella creazione della tabella:", error.message);
+    }
+}
 
 const storicoTimbratureVisitatoriNavList = document.getElementById("storico-timbrature-visitatori");
 storicoTimbratureVisitatoriNavList.addEventListener("click", createStrocioTimbratureVisitatoriDataTable);
